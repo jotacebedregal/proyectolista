537fbdef786069a5920a45ca34df8ed1059ac45f
jotacebedregal <julio.bedregal@ucsp.edu.pe>
2016-05-28 13:24:58 -0500

    commit
    ordenar lista

--- AlumnoRepositorio.java
+++ AlumnoRepositorio.java
@@ -11,8 +11,42 @@ public class AlumnoRepositorio {
 		new Alumno("Maria", "Castro", 14),
 		new Alumno("Jose", "Ruiz", 18),
 	};
+
+	public List<Alumno> listarordepromedio() {
+
+	List<Alumno> alumnos=Arrays.asList(data);
+	Collections.sort(alumnos , new Comparator<Alumno>() {
+	@Override
+		public int compare(Alumno a1,Alumno a2) {
+			return a1.promedio.compareTo(a2.promedio);
+		}
+	});
+		return alumnos;
+	}
+
+	List<Alumno> alumnos=Arrays.asList(data);
+	Collections.sort(alumnos , new Comparator<Alumno>() {
+	@Override
+		public int compare(Alumno a1,Alumno a2) {
+			return a1.apellidoPaterno.compareTo(a2.apellidoPaterno);
+		}
+	});
+		return alumnos;
+	}
+
+	List<Alumno> alumnos=Arrays.asList(data);
+	Collections.sort(alumnos , new Comparator<Alumno>() {
+	@Override
+		public int compare(Alumno a1,Alumno a2) {
+			return a1.nombres.compareTo(a2.nombres);
+		}
+	});
+		return alumnos;
+	}
 	
 	public List<Alumno> listar() {
 		return Arrays.asList(data);
 	}
+
+
 }
